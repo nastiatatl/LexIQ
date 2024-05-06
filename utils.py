77a173377a67ac78320cea38555450d93159c7b2
Anastasiia Tatlubaeva <tatlubaevaa@gmail.com>
2024-05-06 18:57:06 -0400

    remove synonym generation package

--- utils.py
+++ utils.py
@@ -1,18 +1,4 @@
-from wordhoard import Synonyms
 from typing import List
-from typeguard import typechecked
-import random
-
-
-@typechecked
-def generate_synonyms(word: str) -> str:
-    # FIXME: synonyms are in alphabetical order, for now sample randomly
-    # in the future, might select the ones with the highest similarity scores
-    # might take too long though
-    # NOTE: need to take care of part-of-speech mismatch, potential test case
-    all_synonyms = Synonyms(search_string=word).find_synonyms()
-    random_synonym = all_synonyms[random.randint(0, len(all_synonyms) - 1)]
-    return random_synonym
 
 def process_input(input_str: str) -> List[str]:
     # NOTE: potential unit test: various inputs
@@ -29,9 +15,5 @@ def create_score_message(score: int, total_questions: int) -> str:
               "NOT BAD!!!" if percentage >= 25 else \
               "NEED MORE PRACTICE!!!" if score > 0 else ":("
     return message
-
-if __name__ == '__main__':
-    from nltk.corpus import wordnet as wn
-    print(wn.synonyms('strong'))
 
 
