d8f6dc777bafdf36e1650f45f6b3e0d3e0e844ed
Timothy Obiso <9258849+timothyobiso@users.noreply.github.com>
2024-05-06 23:35:19 -0400

    store questions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
+from flask_sqlalchemy import SQLAlchemy
 from utils import process_input, create_score_message
 from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
@@ -15,7 +16,27 @@ app.secret_key = os.getenv("OPENAI_API_KEY")
 
 # folder 'uploads' should be in the root directory
 app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "7843dfhjbsiursuv4r7n4e8yvni8409"
+db = SQLAlchemy(app)
 
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(500), nullable=False)
+    option1 = db.Column(db.String(100), nullable=False)
+    option2 = db.Column(db.String(100), nullable=False)
+    option3 = db.Column(db.String(100), nullable=False)
+    option4 = db.Column(db.String(100), nullable=False)
+    correct_answer = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Question('{self.text}', '{self.option1}', '{self.option2}', '{self.option3}', '{self.option4}', '{self.correct_answer}')"
+
+
+with app.app_context():
+    db.create_all()
 
 # landing page
 @app.route('/')
@@ -60,15 +81,25 @@ def save_words():
     # split into question and answer choices
     model_response = [q.split("\n") for q in model_response]
 
+    # remove number from question if present (example: 1. how are you? -> how are you?)
+    model_response = [[q[0].split(".")[1].strip()] + q[1:] for q in model_response]
+
+
     # package questions and send to page
     old_questions = [{"question": q[0].replace(session['words'][i], "__________"),
                       "options": random.sample([_.strip().split(" ")[-1] for _ in q[1:]] + [session['words'][i]],
                                                len(q)),
                       "answer": session['words'][i]} for i, q in enumerate(model_response)]
-
+    db_questions = [Question(text=q["question"],
+                             option1=q["options"][0],
+                             option2=q["options"][1],
+                             option3=q["options"][2],
+                             option4=q["options"][3],
+                             correct_answer=q["answer"]) for q in old_questions]
+    db.session.add_all(db_questions)
+    db.session.commit()
     # store questions
     session['questions'] = [{"id": i,
-                             "data": question,
                              "answer": question["answer"],
                              "question": question["question"],
                              "options": question["options"]}
@@ -80,14 +111,51 @@ def save_words():
     return render_template('display_words.html', word_synonyms_pairs=words_options)
 
 
+# page to display stored questions
+@app.route('/display_questions')
+def display_questions():
+    # retrieve questions from database
+    questions = Question.query.all()
+    print(questions)
+    return render_template('display_questions.html', questions=questions)
+
+@app.route('/delete_question/<int:qid>', methods=['POST', 'GET'])
+def delete_question(qid):
+    # delete a question from the database
+    question = Question.query.get(qid)
+    db.session.delete(question)
+    db.session.commit()
+    return redirect('/display_questions')
+
+
 @app.route('/quiz', methods=['POST', 'GET'])
-def generate_quiz():
+def quiz():
     # generate quiz from stored questions
     questions = session['questions']
     return render_template('quiz.html', questions=questions)
 
+@app.route('/saved_quiz', methods=['POST'])
+def saved_quiz():
+    responses = []
+    print(request.form.keys())
+    for key in request.form.keys():
+        if key.startswith('add'):
+            responses.append(request.form.getlist(key)[0])
 
-@app.route('/submit-quiz', methods=['POST'])
+    # create a quiz with the questions with the IDs in responses from sqlite
+    db_questions = Question.query.all()
+    for q in db_questions:
+        print(q.id)
+    print(responses)
+    questions = [{"question": q.text,
+                  "options": [q.option1, q.option2, q.option3, q.option4],
+                  "answer": q.correct_answer} for q in db_questions if str(q.id) in responses]
+
+
+    return render_template('quiz.html', questions=questions)
+
+
+@app.route('/submit_quiz', methods=['POST'])
 def submit_quiz():
     # Calculate the score and gather feedback
     score = 0
